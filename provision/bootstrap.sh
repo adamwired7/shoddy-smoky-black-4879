aae2ab5fe5406ea82bff37522a413a83b3df82b6
adamwired7 <adamwired7@gmail.com>
2016-10-31 17:11:17 -0700

    3 ch

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Install packages for Vagrant instance
+# Install packages for Vagrant instance 8888
 
 # Install the latest chef
 curl -L https://chef.io/chef/install.sh | sudo bash
@@ -11,7 +11,6 @@ sudo apt-get update
 curl -sSL https://rvm.io/mpapis.asc | gpg --import
 curl -sSL https://get.rvm.io | bash -s stable --ruby
 
-vagrant_bashrc=/home/vagrant/.bashrc
 echo 'export PATH="$PATH:$HOME/.rvm/bin"' >> $vagrant_bashrc
 source $vagrant_bashrc 
 
@@ -20,3 +19,7 @@ gem install bundler
 
 
 sudo apt-get -y install mysql-server-5.5
+
+
+
+this is 1
